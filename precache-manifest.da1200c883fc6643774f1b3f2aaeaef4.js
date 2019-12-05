7d37dc4ea375017f5da715473617d983a0c8adb3
Chris Cates <hello@chriscates.ca>
2019-12-04 19:16:52 -0500

    Updates

---
+++ precache-manifest.da1200c883fc6643774f1b3f2aaeaef4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a53cb46486cb4cea1af2ad0d4d30041",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3445fbd18fae3c449ff2",
+    "url": "/static/css/main.df82dc9d.chunk.css"
+  },
+  {
+    "revision": "add83d6404d7a83633e8",
+    "url": "/static/js/2.1aca80b3.chunk.js"
+  },
+  {
+    "revision": "3445fbd18fae3c449ff2",
+    "url": "/static/js/main.49d3b744.chunk.js"
+  },
+  {
+    "revision": "66ec99c66fc63932591b",
+    "url": "/static/js/runtime-main.0cc5ba3c.js"
+  }
+]);
